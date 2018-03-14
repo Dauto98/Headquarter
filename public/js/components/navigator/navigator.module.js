38a5d717386840ebe24bd56aced2a1e83bdb382e
Dauto98 <dauto98@gmail.com>
2018-03-14 19:31:08 +0700

    delete code, refresh for react

--- public/js/components/navigator/navigator.module.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-import angular from 'angular';
-import uirouter from '@uirouter/angularjs';
-import navigatorComponent from './navigator.component.js';
-
-export default angular.module('navigator', [uirouter]).component("navigator", navigatorComponent);
